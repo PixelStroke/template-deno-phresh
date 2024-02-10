1f154b34bc1fc600da6ffe5644f1c3895b295102
Shon Thomas <shonpt@live.com>
2024-02-10 13:04:09 +0000

    update title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>template-phaser-denod</title>
+        <title>template-phaser-phresh</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body className={"bg-black"}>
